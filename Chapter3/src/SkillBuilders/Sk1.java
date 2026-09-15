7a8de447f8c3479238977ee8b8348a289cef6ed2
510341003 <510341003@L815WD220029.edu.cbe.ab.ca>
2026-09-15 09:48:50 -0600

    9:43AM

---
+++ Chapter3/src/SkillBuilders/Sk1.java
@@ -0,0 +1,20 @@
+package SkillBuilders;
+
+import java.util.Scanner;
+
+public class Sk1
+{
+
+	public static void main(String[] args) 
+	{
+		//create the loop and i and get the loop to add 2 every time
+		for(int i = 2; i <= 20; i += 2) {
+           
+			//print i as long every loop as long as i is equal or less than 20
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
