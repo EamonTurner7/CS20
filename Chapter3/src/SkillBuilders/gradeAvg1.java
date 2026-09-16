f115782448d1f22ede34f908580ddbf59e0f9dd1
510341003 <510341003@L815WD220029.edu.cbe.ab.ca>
2026-09-16 11:43:59 -0600

    11:43 AM

--- Chapter3/src/SkillBuilders/gradeAvg1.java
+++ Chapter3/src/SkillBuilders/gradeAvg1.java
@@ -6,28 +6,28 @@ public class gradeAvg1 {
 
 	public static void main(String[] args) {
 		
-		
+		int total = 0;
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter grade 1: ");
-		int num1 = input.nextInt();
+		total += input.nextInt();
 
 		System.out.print("Enter grade 2: ");
-		int num2 = input.nextInt();
+		total += input.nextInt();
 
 		System.out.print("Enter grade 3: ");
-		int num3 = input.nextInt();
+		total += input.nextInt();
 
 		System.out.print("Enter grade 4: ");
-		int num4 = input.nextInt();
+		total += input.nextInt();
 
 		System.out.print("Enter grade 5: ");
-		int num5 = input.nextInt();
+		total += input.nextInt();
 
-		int avrg = (num1 + num2 + num3 + num4 + num5) / 5;
+		int avrg = total / 5;
 		
 		
-		System.out.print("Your average is: " + avrg);
+		System.out.print("Your average is: " + avrg + "%");
 	}
 
 }
